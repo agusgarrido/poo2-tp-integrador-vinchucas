40b8cb5b1a57fd19cb4c8c55cb4094f25cc7e0bf
leilaferreyra <ferreyraleila0@gmail.com>
2025-06-12 19:14:44 -0300

    .

--- src/opinion/Opinion.java
+++ src/opinion/Opinion.java
@@ -26,14 +26,5 @@ public class Opinion {
 		return fecha;
 	}
 
-	public boolean esDeExperto() {
-		// TODO Auto-generated method stub (LO CREE PARA QUE NO ME ROMPA LEI! PERDON)
-		return false;
-	}
-
-	public TipoOpinion getTipo() {
-		// TODO Auto-generated method stub(TAMBNIEN LO CREE APRA QUE NO ROMPA! ACA DEBERIA DE RETORNAR ALGO DE TIPO DE OPINION DE ENUM)
-		return null;
-	}
 
 }
